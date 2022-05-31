48d26489ab4259df56b4eee41c6a2b19e1b4fe8c
Joel Pimenta <psjoel02@gmail.com>
2022-05-31 09:46:26 -0500

    - Hourly Forecast now displays precipitation chance and windspeed, and has been converted to a vertical layout to more closely mimic Motorola's Widget - "Current details" bar from Google's Widget has been added

--- app/src/main/java/com/example/weather/WeatherRV.kt
+++ app/src/main/java/com/example/weather/WeatherRV.kt
@@ -1,9 +1,9 @@
 package com.example.weather
 
 class WeatherRV(var temperature: String,
-                var icon: Int) {
-
-    //primary constructor created by default in Kotlin
+                var icon: Int,
+                var precip: String,
+                var wind: String) {
 
 
 }
